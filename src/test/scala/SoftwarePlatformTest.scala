309d0aa4acbad7e11dde6dea3a78635fe2e7d2ea
Adam Beck <akb93@case.edu>
2017-10-16 00:33:19 -0400

    Created a test for jobListDuration chained jobs.

--- src/test/scala/SoftwarePlatformTest.scala
+++ src/test/scala/SoftwarePlatformTest.scala
@@ -12,11 +12,31 @@ class SoftwarePlatformTest extends FlatSpec {
 		appendJobToSchedule(job1, schedule)
 		appendJobToSchedule(job2, schedule)
 
-
 		assert(SoftwarePlatform.jobListDuration(schedule) == 15)
 	}
 
+	behavior of "jobListDuration for a job list of chained elements"
+	it should "return the sum of (single element + max{chained elements})" in {
+		val job1 = Job(Set(), 7, 1)
+		val job2 = Job(Set(), 8, 2)
+		val job3 = Job(Set(), 8, 4)
+		val schedule = ListBuffer[ListBuffer[Job]]()
+		appendJobToSchedule(job1, schedule)
+		appendJobToSchedule(job2, schedule)
+		insertJobToSchedule(job3, schedule, 0)
+
+		assert(SoftwarePlatform.jobListDuration(schedule) == 16)
+
+	}
+
+	// Appends a job to the end of a schedule in new separate list
 	def appendJobToSchedule(job: Job, schedule: ListBuffer[ListBuffer[Job]]): Unit = {
 		schedule += ListBuffer[Job](job)
 	}
+
+	// Appends a job to a list of given index in a schedule
+	def insertJobToSchedule(job: Job, schedule: ListBuffer[ListBuffer[Job]], index: Int): Unit = {
+		schedule(index) += job
+	}
+
 }
