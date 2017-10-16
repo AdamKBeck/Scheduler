5d6cef6d6da6b5c5f1c4e4270571edec716f26c7
Adam Beck <akb93@case.edu>
2017-10-16 16:48:38 -0400

    Created a todo for exception handling structure

--- src/main/scala/CircularDependencyException.scala
+++ src/main/scala/CircularDependencyException.scala
@@ -3,5 +3,5 @@ package scheduler
 import scala.collection.mutable.ListBuffer
 
 case class CircularDependencyException(jobList: Set[Job]) extends Exception (jobList.toString){
-
+// TODO: rename as DependencyException modeled after Parser Exception, with verify methods to aid SoftWarePlatform
 }
