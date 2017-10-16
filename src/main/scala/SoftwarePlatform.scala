1e79a5bc774a084c8c24ef03ae73985ae05602a1
Adam Beck <akb93@case.edu>
2017-10-15 21:31:09 -0400

    Converted pseudocode definition of bestValidOrdering to comments and method stub
    Signed-off-by: Adam Beck <akb93@case.edu>

--- src/main/scala/SoftwarePlatform.scala
+++ src/main/scala/SoftwarePlatform.scala
@@ -1,5 +1,7 @@
 package scheduler
 
+import scala.collection.mutable.ListBuffer
+
 case class SoftwarePlatform() {
 
 }
@@ -14,6 +16,15 @@ object SoftwarePlatform {
 	 * when looping through the list.
 	 */
 	def estimatedDeliveryTime(jobs: List[Job]): Int = {
-			???
+		???
 	}
+
+	/* Input: Job 'job', a job list of lists 'schedule', and the duration of the list 'scheduleDuration'
+	 * Output: Returns a job list of lists, the valid ordering of least duration of 'job' into 'schedule'
+	 */
+	def bestValidOrdering(job: Job, schedule: ListBuffer[ListBuffer[Job]], scheduleDuration: Int) {
+		???
+	}
+
+
 }
