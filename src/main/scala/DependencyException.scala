9517ff4cf6b784b243468c97be2bcfc1ceb76144
Adam Beck <akb93@case.edu>
2017-10-17 01:02:45 -0400

    Created a verify method to help with repeated code
    The method will be called a lot in SoftwarePlatform

--- src/main/scala/DependencyException.scala
+++ src/main/scala/DependencyException.scala
@@ -7,6 +7,17 @@ case class DependencyException() {
 object DependencyException {
 	//TODO: verify methods to avoid repeated code
 
+	// Checks whether a job contains a dependency type on a passed job id
+	def verify(job: Job, dependency: Dependency.Type, id: Int): Boolean = {
+		for (d <- job.dependencies) {
+			if (d.dependencyType == dependency && d.dependencyID == id) {
+				return false
+			}
+		}
+		
+		true
+	}
+
 	sealed abstract class ErrorCode(val code: String) extends Exception(code) {
 
 	}
