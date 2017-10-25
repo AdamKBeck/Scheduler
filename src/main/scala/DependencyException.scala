b5678c4a0a23a10262ddc69e7c5f3a562aa8b01f
Adam Beck <akb93@case.edu>
2017-10-25 02:27:48 -0400

    Verify method changed to use exists statemetn

--- src/main/scala/DependencyException.scala
+++ src/main/scala/DependencyException.scala
@@ -9,15 +9,7 @@ object DependencyException {
 
 	// Checks whether a job contains a dependency type on a passed job id
 	def verify(job: Job, dependency: Dependency.Type, id: Int): Boolean = {
-		for (d <- job.dependencies) {
-			if (d.dependencyType == dependency && d.dependencyID == id) {
-				return true
-			}
-		}
-		/* Function is placed here instead of SoftwarePlatform because we may want to throw
-		 * an exception for future use of this project. (e.g. throw an exception from an
-		 * overloaded verify function in here */
-		false
+		job.dependencies.exists(d => d.dependencyType == dependency && d.dependencyID == id)
 	}
 
 	sealed abstract class ErrorCode(val code: String) extends Exception(code) {
