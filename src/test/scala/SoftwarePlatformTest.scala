3cfb9e7dd227d452272fbdf2932cb467b84b7f0e
Adam Beck <akb93@case.edu>
2017-10-26 00:58:10 -0400

    isPrecedingDurationsValid bad and max good data tested

--- src/test/scala/SoftwarePlatformTest.scala
+++ src/test/scala/SoftwarePlatformTest.scala
@@ -191,6 +191,27 @@ class SoftwarePlatformTest extends FlatSpec with BeforeAndAfterEach with Private
 		val validityResult = SoftwarePlatform invokePrivate isPrecedingDurationsValid(job2, job1, schedule, 0)
 		assert(validityResult)
 	}
+
+	// Bad data: subschedule is Nil
+	it should "test with bad data: Nil subschedule" in {
+		val isPrecedingDurationsValid = PrivateMethod[Boolean]('isPrecedingDurationsValid)
+		schedule = clearSchedule
+		val validityResult = SoftwarePlatform invokePrivate isPrecedingDurationsValid(job2, job1, schedule, 0)
+		assert(validityResult)
+	}
+
+	// Good data: max normal configuration, 100 jobs
+	it should "test with max normal configuration: 100 jobs" in {
+		for (i <- 0 to 98) {
+			val job = Job(Set(), 4, i)
+			appendJobToSchedule(job, schedule)
+		}
+		val isPrecedingDurationsValid = PrivateMethod[Boolean]('isPrecedingDurationsValid)
+		val validityResult = SoftwarePlatform invokePrivate isPrecedingDurationsValid(job2, job1, schedule, 0)
+		assert(validityResult)
+	}
+
+
 }
 
 
